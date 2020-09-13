eeb03eec89538dc18f1b6fad78e962c5b9c7dff0
kuberlog <collinalexbell@gmail.com>
2020-09-13 12:21:58 -0500

    simulation working fine

--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import pygame, sys
 import math
 import pygame.time
 from pygame.locals import *
+from collections import deque
 
 PX_PER_CM = 10
 WINDOW_HEIGHT_CM = 50
@@ -47,6 +48,18 @@ class LinkStart(Start):
 
         return ((end[0]-origin[0])*self.offset_percentage[0] + origin[0] + child_start_offset[0], 
                 (end[1]-origin[1])*self.offset_percentage[0] + origin[1] + child_start_offset[1])
+
+class LinearProcedure:
+    def __init__(self,link, angle_delta, step_delta):
+        self.steps = []
+        angle_step_size = angle_delta / step_delta
+        for i in range(step_delta-1):
+            self.steps.append((lambda: link.joint.move(angle_step_size)))
+        # Last step should move the remainder, it may not be a whole number
+        self.steps.append((lambda: link.joint.move(angle_delta-angle_step_size*(step_delta-1))))
+        
+    def get_moves(self):
+        return self.steps
 
 class Joint:
     def __init__(self, parent, child, start_angle):
@@ -117,8 +130,27 @@ class Bot:
         self.right_fingers = Link(3, 90).joint(self.right_wrist, 0)
         self.left_thumb = Link(2, 90).joint(self.left_wrist)
         self.right_thumb = Link(2, 90).joint(self.right_wrist)
-        
+        self.steps = deque()
 
+    def next_step(self):
+        if(len(self.steps) > 0):
+            return self.steps.popleft()
+        else:
+            return []
+
+    def add_moves(self, moves, step_ind):
+        if (len(self.steps) < step_ind + 1 + len(moves)):
+            for i in range(step_ind+1+len(moves) - len(self.steps)):
+                self.steps.append([])
+
+        for i in range(len(moves)):
+            self.steps[i+step_ind].append(moves[i])
+        
+    def step(self):
+        moves = self.next_step()
+        for move in moves:
+            move()
+        self.render()
 
     def render(self):
         self.root.render()
@@ -161,13 +193,18 @@ def run():
     # draw the window onto the screen
     pygame.display.update()
 
+    turn_left_hand_match_book = LinearProcedure(bot.left_wrist, -30, 20)
+    turn_right_hand_match_book = LinearProcedure(bot.right_wrist, 30, 20)
+
+    bot.add_moves(turn_right_hand_match_book.get_moves(), 0)
+    bot.add_moves(turn_left_hand_match_book.get_moves(), 0)
+
     last_render = pygame.time.get_ticks()
     # run the game loop
     while True:
         clock.tick(60)
         windowSurface.fill(WHITE)
-        bot.left_wrist.joint.move(2)
-        bot.render()
+        bot.step()
         book.render()
         pygame.display.update()
         for event in pygame.event.get():
