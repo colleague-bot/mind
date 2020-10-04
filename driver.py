159d5d9b093c287d414c44ef486bd4a130d40e59
kuberlog <collinalexbell@gmail.com>
2020-10-04 19:55:51 +0100

    add a bunch of actuator

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 from adafruit_servokit import ServoKit
 from gpiozero import OutputDevice
 import time
+from threading import Thread
 
 kit = ServoKit(channels=16)
 
@@ -11,7 +12,7 @@ class Stepper:
         self.direction = OutputDevice(directionPin)
         self.pulse = OutputDevice(pulsePin)
         self.pos = 0
-        self.off()
+        self.on()
 
     def step(self):
         self.pulse.toggle()
@@ -23,12 +24,15 @@ class Stepper:
         self.direction.off()
 
     def on(self):
-        self.enable.on()
-
-    def off(self): 
+        # 0 is on
         self.enable.off()
 
+    def off(self): 
+        # 1 is off
+        self.enable.on()
+
     def move(self, angle):
+        print("moving")
         toMove = angle - self.pos
         if(toMove > 0):
             self.dirPos()
@@ -53,9 +57,41 @@ class Bot():
         self.rightShoulder = Stepper(26, 19, 13)
         self.leftShoulder = Stepper(21, 20, 16)
         self.rightElbow = Servo(0)
+        self.rightElbow.move(180)
         self.leftElbow = Servo(1)
+        self.leftElbow.move(0)
+        self.headZ = Servo(2)
+        self.headZ.move(90)
+        self.headY = Servo(3)
+        self.headY.move(90)
+        self.rightFingers = Servo(6)
+        self.rightFingers.move(180)
+        self.rightThumb = Servo(7)
+        self.rightThumb.move(180)
+        self.leftWrist = Servo(5)
+        self.leftWrist.move(180)
+        self.rightWrist = Servo(4)
+        
+
+    def die(self):
+        self.rightShoulder.off()
+        self.leftShoulder.off()
+
 
 bot = Bot()
+
+def dance():
+    for i in range(20):
+        if(i%2 == 0):
+            Thread(target = lambda: bot.rightShoulder.move(38)).start()
+            Thread(target = lambda: bot.rightElbow.move(180-30)).start()
+            Thread(target = lambda: bot.leftElbow.move(30)).start()
+            bot.leftShoulder.move(-38)
+        else:
+            Thread(target = lambda: bot.rightShoulder.move(-38)).start()
+            Thread(target = lambda: bot.rightElbow.move(180)).start()
+            Thread(target = lambda: bot.leftElbow.move(0)).start()
+            bot.leftShoulder.move(38)
 
 
         
